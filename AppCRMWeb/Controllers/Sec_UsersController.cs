8381f2109ec9d21b216c93dff6b4f095e89a88f6
raju.singh <raju.singh@integritycloudsolutions.com>
2026-05-31 22:23:49 +0530

    User profile

---
+++ AppCRMWeb/Controllers/Sec_UsersController.cs
@@ -0,0 +1,22 @@
+﻿using AapRepository;
+using App.Application.ExternalServices.QBDSync;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppCRMWeb.Controllers
+{
+    public class Sec_UsersController : BaseController
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IQuickBooks _quickBooks;
+        public Sec_UsersController(ILogger<HomeController> logger, IUnitOfWork unitOfWork, IHttpContextAccessor httpContext,
+            IQuickBooks quickBooks) : base(unitOfWork, httpContext)
+        {
+            _logger = logger;
+            _quickBooks = quickBooks;
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
