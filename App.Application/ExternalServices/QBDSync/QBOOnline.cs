b58cc54467444f3195c5b13ef7b06dd72a1621ff
raju.singh <raju.singh@integritycloudsolutions.com>
2026-05-31 15:03:44 +0530

    QBOOnline

---
+++ App.Application/ExternalServices/QBDSync/QBOOnline.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Application.ExternalServices.QBDSync
+{
+    public class QBOOnline
+    {
+        public void TEST()
+        {
+            //var oauthClient = new OAuth2Client("clientId", "clientSecret", "redirectUri",Environment.Sandbox);
+            // .Net OAuth2 Client gets the Discovery document as part of OAuth2Client object
+            //string authUrl = oauthClient.GetAuthorizationURL(new List<OidcScopes> { OidcScopes.Accounting});
+
+            //return Redirect(authUrl);
+        }
+    }
+}
